6a9a5d0e69df5e38c63cbea5312af6baef1b4f08
Eric Lau <ericglau@ca.ibm.com>
2020-07-16 22:50:36 -0400

    Add maven plugin

---
+++ jfs-maven-plugin/src/main/java/com/github/ericglau/DeployMojo.java
@@ -0,0 +1,16 @@
+package com.github.ericglau;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name = "deploy", defaultPhase = LifecyclePhase.INITIALIZE)
+public class DeployMojo extends AbstractMojo {
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+}
